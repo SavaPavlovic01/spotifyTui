34695de7a2501776f87d98cc23e8688ce7206c41
Sava Pavlovic <savapavlovic6@gmail.com>
2025-08-06 20:13:37 +0200

    added argument parsing

--- main.go
+++ main.go
@@ -1,18 +1,46 @@
 package main
 
 import (
+	"os"
+	"strings"
+
 	"example.com/m/v2/auth"
 	"example.com/m/v2/commands"
 )
+
+func ParseInput() []string {
+	var query strings.Builder
+	ret := []string{}
+	i := 2
+	for i < len(os.Args) {
+		if !strings.HasPrefix(os.Args[i], "-") {
+			break
+		}
+		ret = append(ret, os.Args[i])
+		i += 1
+	}
+
+	for i < len(os.Args) {
+		query.WriteString(os.Args[i])
+		query.WriteString(" ")
+		i += 1
+	}
+
+	return append([]string{query.String()}, ret...)
+}
 
 func main() {
 	tokens, err := auth.LoadTokens()
 	if err != nil {
 		tokens = auth.InitialAuth()
 	}
-	//_ = commands.CommandRegistry["s"].Exec(tokens, []string{})
-	err = commands.CommandRegistry["saa"].Exec(tokens, []string{"dev lemons"})
 
+	input := ParseInput()
+	if len(os.Args) == 1 {
+		panic("Pls send a command")
+	}
+
+	err = commands.CommandRegistry[os.Args[1]].Exec(tokens, input)
 	if err != nil {
 		panic(err)
 	}
